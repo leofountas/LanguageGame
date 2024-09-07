dee58c659bcbff42ba9b34e20e129631d55e085e
leofountas <leofoutas@gmail.com>
2024-09-07 03:54:06 +0200

    view kind of completed

--- Model/Player.php
+++ Model/Player.php
@@ -4,7 +4,6 @@ namespace Model;
 
 class Player
 {
-
     
     public function __construct(public string $name, public int $score)
     {
@@ -12,5 +11,15 @@ class Player
         $this->score = 0;
     }
 
-    
+    public function show_name() {
+        return $this->name;
+    }
+
+    public function show_score() {
+        return $this->score;
+    }
+
+    public function increase_score() {
+        $this->score ++;
+    }
 }
